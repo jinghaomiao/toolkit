1e274836d27b86a5dabeed9741847fd2c568dde3
Aaron Xiao <xiaoxiangquan@gmail.com>
2014-09-24 17:07:31 +0800

    ADD: normalize_csv can make a csv file contain exactly n fields

---
+++ normalize_csv.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import sys, csv
+
+def scan_csv(in_csv, out_csv, field_count):
+  in_csv = file(in_csv, 'r')
+  out_csv = file(out_csv, 'w')
+  writer = csv.writer(out_csv)
+  for line in csv.reader(in_csv):
+    if len(line) < field_count:
+      print "Strange record:", line
+      continue
+    writer.writerow(line[:6])
+  in_csv.close()
+  out_csv.close()
+
+def main():
+  if len(sys.argv) != 4:
+    print 'Usage: ' + sys.argv[0] + ' <input_csv> <output_csv> <keep_fields_count>'
+    exit()
+  scan_csv(sys.argv[1], sys.argv[2], int(sys.argv[3]))
+
+if __name__ == '__main__':
+  main()
