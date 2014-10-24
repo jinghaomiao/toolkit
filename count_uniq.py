ed55ddecdf218c83c68f05dcfbefef691095ddfd
Aaron Xiao <xiaoxiangquan@gmail.com>
2014-10-24 10:37:52 +0800

    Add count_uniq.py that sort, count unique lines

---
+++ count_uniq.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import sys
+
+def scan(input_file):
+  count = {}
+  for line in file(input_file):
+    line = line[:-1]
+    if count.has_key(line):
+      count[line] += 1
+    else:
+      count[line] = 1
+
+  reversed_count = [(line, count[line]) for line in count]
+  reversed_count.sort(key = lambda x : x[1], reverse = True)
+
+  for line, cnt in reversed_count:
+    print cnt, line
+
+def main():
+  if len(sys.argv) != 2:
+    print 'Usage: ' + sys.argv[0] + ' <input_file>'
+    print '    It\'s like `cat $input_file | sort | uniq`, but appending count number on each line'
+    exit()
+  scan(sys.argv[1])
+
+if __name__ == '__main__':
+  main()
