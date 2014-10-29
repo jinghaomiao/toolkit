98350251e180451ee19acbba80b21a75591aa29e
Aaron Xiao <xiaoxiangquan@gmail.com>
2014-10-29 16:14:51 +0800

    Draw CDF with a series of numbers

--- numbers_to_cdf.py
+++ numbers_to_cdf.py
@@ -3,12 +3,12 @@
 import sys
 import matplotlib.pyplot as plt
 
-numbers = []
 for line in sys.stdin:
+  numbers = []
   for num in line.split():
     numbers.append(float(num))
-numbers.sort()
-count = [i for i in range(1, len(numbers)+1)]
+  numbers.sort()
+  count = [i for i in range(1, len(numbers)+1)]
+  plt.plot(numbers, count)
 
-plt.plot(numbers, count)
 plt.show()
