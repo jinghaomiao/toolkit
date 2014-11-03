35f63a4d6be33698edfbb0002bf1a41306542ff9
Aaron Xiao <aaronx@google.com>
2014-11-03 09:52:40 +0800

    Remove a tag and its content from html string.

--- remove_tag_from_html.py
+++ remove_tag_from_html.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 import sys
 from HTMLParser import HTMLParser
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 ################################ HELP
 def help():
