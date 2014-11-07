6c85ff96b033e1d40ab65d2bf542767a09905266
Aaron Xiao <xiaoxiangquan@gmail.com>
2014-11-07 14:26:03 +0800

    Works like `sort  | uniq`, but pre-appends occurrence count on each line.

--- count_uniq.py
+++ count_uniq.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python
 import sys
+import fileinput
 
-def scan(input_file):
-  count = {}
-  for line in file(input_file):
-    line = line[:-1]
-    if count.has_key(line):
-      count[line] += 1
-    else:
-      count[line] = 1
+################################ HELP
+def help():
+  print 'Usage:', sys.argv[0], '[input_file]'
+  print '''\
+    Works like `sort $input_file | uniq`, but pre-appends occurrence count on each line,
+    and outputs in decrease order
+    <input_file>    File that contains data. STDIN will be used if not specified.'''
+  exit()
 
-  reversed_count = [(line, count[line]) for line in count]
-  reversed_count.sort(key = lambda x : x[1], reverse = True)
+if len(sys.argv) == 2 and (sys.argv[1] == '-h' or sys.argv[1] == '--help'):
+  help()
 
-  for line, cnt in reversed_count:
-    print cnt, line
+################################ PROCESS
+count = {}
+for line in fileinput.input():
+  if count.has_key(line):
+    count[line] += 1
+  else:
+    count[line] = 1
 
-def main():
-  if len(sys.argv) != 2:
-    print 'Usage: ' + sys.argv[0] + ' <input_file>'
-    print '    It works like `sort $input_file | uniq`, but pre-appends occurrence count on each line, and outputs in decrease order'
-    exit()
-  scan(sys.argv[1])
+reversed_count = [(line, count[line]) for line in count]
+reversed_count.sort(key = lambda x : x[1], reverse = True)
 
-if __name__ == '__main__':
-  main()
+for line, cnt in reversed_count:
+  print cnt, line,
