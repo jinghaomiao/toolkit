e422eb7b34aa6c2d81a1984517e11db7bc68800f
Aaron Xiao <aaronx@google.com>
2014-12-31 13:56:43 +0800

    Rename files with a pattern.

--- rename_files.py
+++ rename_files.py
@@ -1,24 +1,32 @@
+#!/usr/bin/env python
 import sys
 import os
 
-# Usage:
-# ./rename.py <root_dir_name> <rename_from> <rename_to>
-# e.g. ./rename.py ./ " " "_SPACE_"
+if len(sys.argv) < 4:
+  print 'Usage: %s <root_dir_name> <rename_from> <rename_to> [levels]' % sys.argv[0]
+  print 'e.g. ./rename.py ./ " " "_SPACE_"'
+  exit()
 
 root = sys.argv[1]
 rename_from = sys.argv[2]
 rename_to = sys.argv[3]
+levels = 99999999
+if len(sys.argv) == 5:
+  levels = int(sys.argv[4])
 
-def rename(dir_name):
+def rename(dir_name, levels):
+  if levels = 0:
+    return
+
   for from_name in os.listdir(dir_name):
     full_name = os.path.join(dir_name, from_name)
     if os.path.isdir(full_name):
-      rename(full_name)
+      rename(full_name, levels - 1)
 
     to_name = from_name.replace(rename_from, rename_to)
     if from_name != to_name:
       to_name = os.path.join(dir_name, to_name)
       os.rename(full_name, to_name)
 
-rename(root)
+rename(root, levels)
 
