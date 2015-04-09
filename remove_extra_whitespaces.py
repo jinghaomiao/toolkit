b4c0325913a65367fa03bbe550880ec9894c902b
Aaron Xiao <xiaoxiangquan@gmail.com>
2015-04-09 14:13:37 +0800

    Remove extra whitespaces.

--- remove_extra_whitespaces.py
+++ remove_extra_whitespaces.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 import fileinput
 import sys
-import re
 ################################ HELP
 def help():
   print 'Usage:', sys.argv[0], '[input_file]'
@@ -14,7 +13,6 @@ if len(sys.argv) == 2 and (sys.argv[1] == '-h' or sys.argv[1] == '--help'):
   help()
 
 ################################ PROCESS
-regex = re.compile(r'\s+')
 for line in fileinput.input():
-  print re.sub(regex, ' ', line)
+  print ' '.join(line.split())
 
