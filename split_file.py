71157a0b394c568d753d4fc3ecca4be1134d066e
Aaron Xiao <xiaoxiangquan@gmail.com>
2015-04-09 14:45:02 +0800

    Split the STDIN to sub-files by limiting lines or sub-file count.

--- split_file.py
+++ split_file.py
@@ -4,13 +4,11 @@ import math
 
 ################################ HELP
 def help():
-  print 'Usage: %s <output_prefix> [-l lines_per_file]|[-n files_count]|[-m max_lines_per_file]'
+  print 'Usage: %s [-l lines_per_file]|[-n files_count]|[-m max_lines_per_file] <output_prefix [output_postfix]>'
   print '''    Split the STDIN to sub-files.'''
   exit()
 
-if len(sys.argv) == 2 and (sys.argv[1] == '-h' or sys.argv[1] == '--help'):
-  help()
-elif len(sys.argv) != 4:
+if len(sys.argv) < 4:
   help()
 
 ################################ FUNCTIONS
@@ -30,21 +28,24 @@ def produce_files(lines, output_format, number_of_lines):
   output.close()
 
 ################################ PROCESS
-output_prefix = sys.argv[1]
+output_prefix = sys.argv[3]
 lines = [line for line in sys.stdin]
 lines_count = len(lines)
-if (sys.argv[2] == '-l'):
-  number_of_lines = int(sys.argv[3])
+if (sys.argv[1] == '-l'):
+  number_of_lines = int(sys.argv[2])
   number_of_files = (lines_count + number_of_lines - 1) / number_of_lines
-elif (sys.argv[2] == '-n'):
-  number_of_files = int(sys.argv[3])
+elif (sys.argv[1] == '-n'):
+  number_of_files = int(sys.argv[2])
   number_of_lines = (lines_count + number_of_files - 1) / number_of_files
-elif (sys.argv[2] == '-m'):
-  max_lines_per_file = int(sys.argv[3])
+elif (sys.argv[1] == '-m'):
+  max_lines_per_file = int(sys.argv[2])
   number_of_files = (lines_count + max_lines_per_file - 1) / max_lines_per_file
   number_of_lines = (lines_count + number_of_files - 1) / number_of_files
 
 number_of_files_width = int(math.log10(number_of_files))+1
 output_format = '%s_%%0%dd_of_%d' % (output_prefix, number_of_files_width, number_of_files)
+if len(sys.argv) == 5:
+  output += '.' + sys.argv[4]
 
 produce_files(lines, output_format, number_of_lines)
+
