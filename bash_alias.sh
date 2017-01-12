03b64a2aec60a04efecd11b3bd276897a2e86842
Aaron Xiao <xiaoxiangquan@baidu.com>
2017-01-12 13:43:00 -0800

    MOD: gm = git commit -m

--- bash_alias.sh
+++ bash_alias.sh
@@ -13,7 +13,7 @@ alias p='ps aux | grep -i'
 
 # git
 alias gs='git status'
-alias gc='git commit -m'
+alias gm='git commit -m'
 alias ga='git commit --amend --no-edit && git push origin HEAD:refs/for/master'
 alias gp='git push origin HEAD:refs/for/master'
 
