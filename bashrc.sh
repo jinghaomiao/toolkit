e24780423f5db9427f431942731edbb3a6f2ed86
Aaron Xiao <xiaoxiangquan@gmail.com>
2017-02-10 16:59:35 -0800

    ADD: python venv setup tools.

--- bashrc.sh
+++ bashrc.sh
@@ -11,6 +11,12 @@ alias l='ls -CF'
 # ps
 alias p='ps aux | grep -i'
 
+# python
+alias py2='source $WS/toolkit/pyenv/py2.7-std/bin/activate'
+alias py3='source $WS/toolkit/pyenv/py3.6-std/bin/activate'
+alias py='python'
+alias bpy='bpython'
+
 # git
 alias gs='git status'
 alias ga='git commit --amend --no-edit && git push origin HEAD:refs/for/master'
