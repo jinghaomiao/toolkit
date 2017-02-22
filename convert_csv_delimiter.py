d307fcc1ce1eca8d0c161a4ac6d0595bcf65c3d5
Aaron Xiao <xiaoxiangquan@gmail.com>
2017-02-21 17:17:16 -0800

    DEL: Unused tools.

--- convert_csv_delimiter.py
+++ convert_csv_delimiter.py
@@ -1,19 +1,18 @@
-#!/usr/bin/python
-import sys, csv
+#!/usr/bin/env python
+import csv
+import sys
 
 def scan_csv(in_csv, delimiter):
-  in_csv = file(in_csv, 'r')
-  for line in csv.reader(in_csv):
-    print delimiter.join(line)
-  in_csv.close()
+  with open(in_csv, 'r') as f:
+    for line in csv.reader(f):
+      print delimiter.join(line)
 
 def main():
   if len(sys.argv) != 3:
-    print 'Usage: ' + sys.argv[0] + ' <input_csv> <delimiter>'
-    print '    Output csv data with the specified delimiter'
-    exit()
+    print '''Usage: {} <input_csv> <delimiter>
+        Output csv data with the specified delimiter'''.format(sys.argv[0])
+    exit(1)
   scan_csv(sys.argv[1], sys.argv[2])
 
 if __name__ == '__main__':
   main()
-
