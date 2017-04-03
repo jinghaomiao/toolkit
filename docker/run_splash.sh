4e2950901a7e6795771679978150122a3f0a68c8
Aaron Xiao <xiaoxiangquan@baidu.com>
2017-04-03 16:47:39 -0700

    ADD: Splash container.

--- docker/run_splash.sh
+++ docker/run_splash.sh
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 #!/bin/sh
 
 sudo docker run -d -p 5023:5023 -p 8050:8050 -p 8051:8051 scrapinghub/splash
+=======
+#!/bin/bash
+
+sudo docker run -d --name=splash -p 5023:5023 -p 8050:8050 -p 8051:8051 scrapinghub/splash
+>>>>>>> ADD: Splash container.
