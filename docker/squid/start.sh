c9de51c02da29b6a9fe95b7287e732457d5225bc
Aaron Xiao <xiaoxiangquan@gmail.com>
2017-04-24 20:54:33 -0700

    MOD: Update squid to 3.3.8-23.

--- docker/squid/start.sh
+++ docker/squid/start.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+pushd $(dirname $0)
+
 CACHE=$(pwd)/.cache
 CONF=$(pwd)/squid.conf
 
 mkdir -p $CACHE
 docker run --name squid -d --restart=always \
-  --publish 8002:3128 \
+  --publish 8999:3128 \
   --volume $CONF:/etc/squid3/squid.conf \
   --volume $CACHE:/var/spool/squid3 \
-  sameersbn/squid:3.3.8-20
+  sameersbn/squid:3.3.8-23
+
+popd
 
