17113a8807c99f95a44d266985571961ae0871d5
Aaron Xiao <xiaoxiangquan@baidu.com>
2017-05-30 15:01:59 -0700

    MOD: Refactor global gitignore.

--- docker/squid/run_squid.sh
+++ docker/squid/run_squid.sh
@@ -6,7 +6,8 @@ CONF=$(pwd)/squid.conf
 cd $(dirname $0)
 mkdir $CACHE
 docker run --name squid -d --restart=always \
-  -p 8999:3128 \
+  -p 8128:3128 \
   -v $CONF:/etc/squid3/squid.conf \
   -v $CACHE:/var/spool/squid3 \
   sameersbn/squid:3.3.8-23
+
