958f9876f9024884326239d8d85a0b5c84c2c0b5
Aaron Xiao <xiaoxiangquan@gmail.com>
2017-07-17 15:45:24 -0700

    Update configs.

--- configs/bashrc.sh
+++ configs/bashrc.sh
@@ -41,3 +41,4 @@ alias br='bazel run'
 
 # Ubuntu
 alias uu='sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove && sudo apt-get autoremove --purge'
+alias aptSearch='apt-cache search'
