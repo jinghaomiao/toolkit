65ad067c80e38a7ea317a8262098fc36aa5611b8
Aaron Xiao <xiaoxiangquan@gmail.com>
2018-09-01 17:00:30 -0700

    Simplify scripts.

--- docker/dev-env/login.sh
+++ docker/dev-env/login.sh
@@ -2,7 +2,5 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 source installers/ENV.sh
 
-DOCKERFILE=$(basename $1)
-TAG=$(echo ${DOCKERFILE} | sed 's/\.Dockerfile$//g')
-
+TAG=$1
 docker exec -it -u ${USER} ${TAG} bash
--- docker/dev-env/start.sh
+++ docker/dev-env/start.sh
@@ -2,17 +2,19 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 source installers/ENV.sh
 
-DOCKERFILE=$(basename $1)
-TAG=$(echo ${DOCKERFILE} | sed 's/\.Dockerfile$//g')
+TAG=$1
 IMAGE=${DOCKER_REPO}:${TAG}
 
 set -x
 docker rm -f ${TAG} 2> /dev/null || true
-docker run -it --rm \
+docker run -it -d \
     --net=host --privileged \
     --name=${TAG} \
     --hostname ${TAG} \
     --add-host ${TAG}:127.0.0.1 \
     -v $(realpath ~/work):/home/${USER}/work \
+    -v ~/.ssh:/home/${USER}/.ssh \
     ${IMAGE}
 set +x
+
+bash login.sh ${TAG}
