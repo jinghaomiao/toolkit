047ea0823b3e1ad0c2617605009d056b7421dbd7
Aaron Xiao <xiaoxiangquan@gmail.com>
2018-09-04 11:53:36 -0700

    Retire expired code.

--- docker/bazel-grpc-cache-server/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker build --network=host \
-    -t xiangquan/bazel-grpc-cache-server:latest \
-    $(dirname $0)
--- docker/bazel-grpc-cache-server/install/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-
-/home/install/bazel-master/bazel-bin/src/tools/remote_worker/remote_worker \
-    --listen_port=${PORT}
